004bb32ac14c4a28e6064738c7bfe9a5345e744c
Huzayfah Patel <huzayfah.patel@outlook.com>
2023-05-22 18:17:29 +0100

    assigning private keys

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -1,6 +1,28 @@
-const {secp256k1} = require('ethereum-cryptography/secp256k1')
-const {toHex} = require('ethereum-cryptography/utils')
+const { secp256k1 } = require('ethereum-cryptography/secp256k1');
+const { toHex } = require('ethereum-cryptography/utils');
 
-const privateKey = secp256k1.utils.randomPrivateKey()
+// Generate private keys for Alice, Ben, and Bob
+const alicePrivateKey = secp256k1.utils.randomPrivateKey();
+const benPrivateKey = secp256k1.utils.randomPrivateKey();
+const bobPrivateKey = secp256k1.utils.randomPrivateKey();
 
-console.log("private key: ", toHex(privateKey))
+// Derive public keys from the private keys
+const alicePublicKey = secp256k1.getPublicKey(alicePrivateKey);
+const benPublicKey = secp256k1.getPublicKey(benPrivateKey);
+const bobPublicKey = secp256k1.getPublicKey(bobPrivateKey);
+
+// Convert private and public keys to hexadecimal strings
+const alicePrivateKeyHex = toHex(alicePrivateKey);
+const benPrivateKeyHex = toHex(benPrivateKey);
+const bobPrivateKeyHex = toHex(bobPrivateKey);
+const alicePublicKeyHex = toHex(alicePublicKey);
+const benPublicKeyHex = toHex(benPublicKey);
+const bobPublicKeyHex = toHex(bobPublicKey);
+
+// Print the private and public keys for Alice, Ben, and Bob
+console.log('Alice public key:', alicePublicKeyHex);
+console.log('Alice private key:', alicePrivateKeyHex);
+console.log('Ben public key:', benPublicKeyHex);
+console.log('Ben private key:', benPrivateKeyHex);
+console.log('Bob public key:', bobPublicKeyHex);
+console.log('Bob private key:', bobPrivateKeyHex);
