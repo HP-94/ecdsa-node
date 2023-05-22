6647c7b9ef5f490d33ab261110e257b29fb9ce1e
Huzayfah Patel <huzayfah.patel@outlook.com>
2023-05-22 17:11:11 +0100

    attempting to fix error where randomPrivateKey not recognised

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -1,4 +1,6 @@
-const secp = require("ethereum-cryptography/secp256k1");
+import { secp256k1 } from "ethereum-cryptography/secp256k1.js";
+import { toHex } from "ethereum-cryptography/utils.js";
 
+const privateKey = secp256k1.randomPrivateKey();
 
-
+console.log('private key', toHex(privateKey));
